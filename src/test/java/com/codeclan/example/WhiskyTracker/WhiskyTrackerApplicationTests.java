6cd59c3872df5868f3735ae31b3a926610c92800
Timothy Saudners <timsaunders88@gmail.com>
2020-10-07 21:25:19 +0100

    added findDistilleriesByRegion to distillery repo and added test

--- src/test/java/com/codeclan/example/WhiskyTracker/WhiskyTrackerApplicationTests.java
+++ src/test/java/com/codeclan/example/WhiskyTracker/WhiskyTrackerApplicationTests.java
@@ -1,10 +1,13 @@
 package com.codeclan.example.WhiskyTracker;
 
+import com.codeclan.example.WhiskyTracker.models.Distillery;
 import com.codeclan.example.WhiskyTracker.models.Whisky;
+import com.codeclan.example.WhiskyTracker.repositories.DistilleryRepository;
 import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -19,6 +22,9 @@ public class WhiskyTrackerApplicationTests {
 	@Autowired
 	WhiskyRepository whiskyRepository;
 
+	@Autowired
+	DistilleryRepository distilleryRepository;
+
 	@Test
 	public void contextLoads() {
 	}
@@ -27,6 +33,12 @@ public class WhiskyTrackerApplicationTests {
 	public void canGetWhiskiesFromYear(){
 		List<Whisky> found = whiskyRepository.findWhiskyByYear(2018);
 		assertEquals(6, found.size());
+	}
+
+	@Test
+	public void canGetDistilleriesFromRegion(){
+		List<Distillery> found = distilleryRepository.findDistilleryByRegion("Speyside");
+		assertEquals(3, found.size());
 	}
 
 }
