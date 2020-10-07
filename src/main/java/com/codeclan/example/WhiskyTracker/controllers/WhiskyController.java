2950c7ed28a9ad4bef4ba191f678968d48dc2ad8
Timothy Saudners <timsaunders88@gmail.com>
2020-10-07 21:12:47 +0100

    added /whiskys route to whiskycontroller taking in a year requestparam

--- src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
@@ -1,13 +1,25 @@
 package com.codeclan.example.WhiskyTracker.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class WhiskyController {
 
-    @GetMapping
-    
+    @Autowired
+    WhiskyRepository whiskyRepository;
+
+    @GetMapping(value = "/whiskys")
+    public ResponseEntity getWhiskysByYear(
+            @RequestParam(name = "year", required = false) Integer year){
+        if (year != null){
+            return new ResponseEntity<>(whiskyRepository.findWhiskyByYear(year), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(whiskyRepository.findAll(), HttpStatus.OK);
+        }
+    }
 
 }
