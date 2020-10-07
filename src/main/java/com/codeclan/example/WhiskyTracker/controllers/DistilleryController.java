d327a75b27548df39a442492c187e7f4a63e6dc3
Timothy Saudners <timsaunders88@gmail.com>
2020-10-07 21:28:29 +0100

    added route for distillaries including requestparam for region

--- src/main/java/com/codeclan/example/WhiskyTracker/controllers/DistilleryController.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/controllers/DistilleryController.java
@@ -1,11 +1,27 @@
 package com.codeclan.example.WhiskyTracker.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.codeclan.example.WhiskyTracker.repositories.DistilleryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DistilleryController {
 
+    @Autowired
+    DistilleryRepository distilleryRepository;
 
+    @GetMapping(value = "/distilleries")
+    public ResponseEntity getDistilleriesByRegion(
+            @RequestParam(name = "region", required = false) String region){
+        if (region != null){
+            return new ResponseEntity<>(distilleryRepository.findDistilleryByRegion(region), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(distilleryRepository.findAll(), HttpStatus.OK);
+        }
+    }
 
 }
